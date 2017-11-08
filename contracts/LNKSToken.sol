ab08bb62c9daf2de66590be1ca6d473b9a800423
Link Platform <steven@ethereum.link>
2017-11-08 17:38:55 -0600

    Update LNKSToken.sol

--- contracts/LNKSToken.sol
+++ contracts/LNKSToken.sol
@@ -167,6 +167,22 @@ contract UpgradeableToken is StandardToken {
   }
  }
 
+contract Controlled {
+    /// @notice The address of the controller is the only address that can call
+    ///  a function with this modifier
+    modifier onlyController { require(msg.sender == controller); _; }
+
+    address public controller;
+
+    function Controlled() public { controller = msg.sender;}
+
+    /// @notice Changes the controller of the contract
+    /// @param _newController The new controller of the contract
+    function changeController(address _newController) public onlyController {
+        controller = _newController;
+    }
+}
+
 function createCloneToken(
         string _cloneTokenName,
         uint8 _cloneDecimalUnits,
