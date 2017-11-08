2bb6284c8c6d2f07b4120a2071655c3689b9c01d
Link Platform <steven@ethereum.link>
2017-11-08 17:35:33 -0600

    Update LNKSVault.sol

--- contracts/LNKSVault.sol
+++ contracts/LNKSVault.sol
@@ -134,6 +134,22 @@ contract LNKSVault is Drainable {
 
 }
 
+contract Controlled {
+    /// @notice The address of the controller is the only address that can call
+    ///  a function with this modifier
+    modifier onlyController { require(msg.sender == controller); _; }
+
+    address public controller;
+
+    function Controlled() public { controller = msg.sender;}
+
+    /// @notice Changes the controller of the contract
+    /// @param _newController The new controller of the contract
+    function changeController(address _newController) public onlyController {
+        controller = _newController;
+    }
+}
+
 contract UpgradeableToken is StandardToken {
 
   /** Contract / person who can set the upgrade path. This can be the same as team multisig wallet, as what it is with its default value. */
